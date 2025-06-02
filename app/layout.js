83939e7928a79b2072793cdb9f20231a19312cda
paal96 <eminem_pro@hotmail.com>
2025-06-02 03:51:57 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import Navbar from '../components/navbar';
 import Footer from '../components/footer';
+import TopBar from '../components/TopBar';
 
 export const metadata = {
   title: 'OSRS Gold Shop',
@@ -11,6 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="bg-gray-900 text-white min-h-screen flex flex-col">
+        <TopBar />
         <Navbar />
         <main className="flex-grow container mx-auto px-4 py-6">
           {children}

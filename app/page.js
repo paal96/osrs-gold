f07cf6de0220309a894c0f9d8a26ae36ad0f795e
paal96 <eminem_pro@hotmail.com>
2025-06-02 18:26:03 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
     <div>
-      <img src="/banner.png" alt="Banner" />
+      <img
+        src="/banner.png"
+        alt="Banner"
+        className="max-w-full h-auto rounded-lg shadow-lg mb-10"
+      />
+      {/* Her kan du legge til mer innhold under banneret */}
     </div>
   );
 }
